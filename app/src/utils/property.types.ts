65de4c1b2e896cd049aabd81c013b5372cdb9092
Bryan Silva <bryanjsilva04@gmail.com>
2024-01-27 20:35:09 -0800

    Added randomness to the properties

--- app/src/utils/property.types.ts
+++ app/src/utils/property.types.ts
@@ -1,9 +1,25 @@
 export type PropertyData = {
     address: string,
-    assessed_total: string,
+    assessed_total: number,
+    assessed_total_formatted: string,
     build_year: string,
-    last_sale_date: string,
-    last_sale_price: string,
+    last_sale_date: number,
+    last_sale_date_formatted: string,
+    last_sale_price: number,
+    last_sale_price_formatted: string,
     latitude: number,
     longitude: number
+}
+
+export const SAMPLE_PROPERTY = {
+    address: "",
+    assessed_total: 0,
+    assessed_total_formatted: "",
+    build_year: "",
+    last_sale_date: 0,
+    last_sale_date_formatted: "",
+    last_sale_price: 0,
+    last_sale_price_formatted: "",
+    latitude: 0,
+    longitude: 0
 }
